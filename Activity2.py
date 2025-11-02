0545515930a579cbafc815f9aa5a0091be25392a
Sharali-25 <Sharaliarkansas@gmail.com>
2025-11-02 09:12:31 -0600

    Add files via upload

---
+++ Activity2.py
@@ -0,0 +1,7 @@
+#Algorithim:add 2 number entered by user
+#Step1:Start
+#Step2:Declare variable ,num1,num2 and sum 
+#Step3:read values num1 and num2
+#Step4:Add num1 and num2 and assign the result to sum
+#Step5:Display sum
+#Step6:Stop
