7f7712410e600e778c49eb01c9c102b062f2581a
Sharali-25 <Sharaliarkansas@gmail.com>
2025-11-08 09:28:32 -0600

    Add files via upload

--- Activity2.py
+++ Activity2.py
@@ -1,7 +1,9 @@
-#Algorithim:add 2 number entered by user
-#Step1:Start
-#Step2:Declare variable ,num1,num2 and sum 
-#Step3:read values num1 and num2
-#Step4:Add num1 and num2 and assign the result to sum
-#Step5:Display sum
-#Step6:Stop
+x=5
+y="Sharali"
+print(x)
+print(y)
+
+codingal="codingal"
+print(codingal)
+name=input("enter ur name: ")
+print("\nhello", name, "\n welcome to codingal")
